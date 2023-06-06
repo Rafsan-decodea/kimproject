a5e13793e46a27b775d2890846c178850b86752d
Your Name <you@example.com>
2023-06-06 14:11:37 +0600

    Working With code

--- testCodes/test.py
+++ testCodes/test.py
@@ -1,28 +1,24 @@
-import pyrebase
-from google.cloud import storage
-import os 
-config = {
-        "apiKey": "AIzaSyBiP96UgQNqzcblfcNqmp8arneThFH7SQI",
-        "authDomain": "kimsirproject.firebaseapp.com",
-        "databaseURL": "https://kimsirproject-default-rtdb.firebaseio.com",
-        "projectId": "kimsirproject",
-        "storageBucket": "kimsirproject.appspot.com",
-        "messagingSenderId": "186797081014",
-        "appId": "1:186797081014:web:d9d08d73bacdd7feb30117",
-        "measurementId": "G-Q268WWQZPN"
-    }
 
-firebase = pyrebase.initialize_app(config)
-base_dir = os.getcwd()
-storage_client = storage.Client.from_service_account_json(base_dir+"/firebase.json")
 
-def fetch_all_images():
-    bucket = storage_client.get_bucket("known")
-    blobs = bucket.list_blobs()
 
-    for blob in blobs:
-        if blob.name.endswith('.jpg') or blob.name.endswith('.jpeg') or blob.name.endswith('.png'):
-            print('Image URL:', blob.public_url)  # Print the download URL
-            # Perform any further processing or display the images as needed
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
 
-fetch_all_images()
+cred = credentials.Certificate("../firebase.json")
+firebase_admin.initialize_app(cred)
+
+def get_bucket(bucket_name):
+    if bucket_name is None:
+        raise ValueError("ValueError: Storage bucket name not specified. Specify the bucket name via the \"storageBucket\" option when initializing the App, or specify the bucket name explicitly when calling the storage.bucket() function.")
+
+
+    bucket = storage.bucket(bucket_name)
+    return bucket
+
+bucket = get_bucket("kimsirproject.appspot.com")
+
+images = bucket.list_blobs()
+
+for image in images:
+    print(image.name)
